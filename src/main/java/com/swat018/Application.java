2e19b1b5127920678d403c08a1f823ebea75685e
jinwoo park <swat018@gmail.com>
2020-05-21 08:37:07 +0900

    자동 설정 만들기 - Starter와 AutoConfigure

--- src/main/java/com/swat018/Application.java
+++ src/main/java/com/swat018/Application.java
@@ -1,20 +1,27 @@
 package com.swat018;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
-       application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
 //        SpringApplication.run(Application.class, args);
     }
+
+   @Bean
+    public Holoman holoman1() {
+        Holoman holoman = new Holoman();
+        holoman.setName("jinwoo");
+        holoman.setHowLong(60);
+        return holoman;
+    }
+
 }
