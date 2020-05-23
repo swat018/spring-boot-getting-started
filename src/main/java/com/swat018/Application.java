838ecdee5d0f5dc8a1f54df16e8bb985ba2b1b76
jinwoo park <swat018@gmail.com>
2020-05-24 04:39:44 +0900

    내장 웹서버 응용 -  HTTPS와 HTTP2

--- src/main/java/com/swat018/Application.java
+++ src/main/java/com/swat018/Application.java
@@ -1,11 +1,7 @@
 package com.swat018;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +30,5 @@ public class Application {
         connector.setPort(8080);
         return connector;
     }*/
-
 
 }
